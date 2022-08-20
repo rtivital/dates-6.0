a8440f7e0cdcd6ae78a2c2e535f0ecd86d0f6cde
Vitaly Rtishchev <rtivital@gmail.com>
2022-08-20 19:17:17 +0300

    Fix incorrect lofic in get start of week handler

--- src/Month/get-start-of-week/get-start-of-week.test.ts
+++ src/Month/get-start-of-week/get-start-of-week.test.ts
@@ -13,5 +13,13 @@ describe('@mantine/dates/get-start-of-week', () => {
     expect(getStartOfWeek(new Date(2021, 1, 5), 2)).toStrictEqual(new Date(2021, 1, 2));
     expect(getStartOfWeek(new Date(2021, 1, 5), 1)).toStrictEqual(new Date(2021, 1, 1));
     expect(getStartOfWeek(new Date(2021, 1, 5), 0)).toStrictEqual(new Date(2021, 0, 31));
+
+    expect(getStartOfWeek(new Date(2022, 1, 15), 6)).toStrictEqual(new Date(2022, 1, 12));
+    expect(getStartOfWeek(new Date(2022, 1, 15), 5)).toStrictEqual(new Date(2022, 1, 11));
+    expect(getStartOfWeek(new Date(2022, 1, 15), 4)).toStrictEqual(new Date(2022, 1, 10));
+    expect(getStartOfWeek(new Date(2022, 1, 15), 3)).toStrictEqual(new Date(2022, 1, 9));
+    expect(getStartOfWeek(new Date(2022, 1, 15), 2)).toStrictEqual(new Date(2022, 1, 15));
+    expect(getStartOfWeek(new Date(2022, 1, 15), 1)).toStrictEqual(new Date(2022, 1, 14));
+    expect(getStartOfWeek(new Date(2022, 1, 15), 0)).toStrictEqual(new Date(2022, 1, 13));
   });
 });
--- src/Month/get-start-of-week/get-start-of-week.ts
+++ src/Month/get-start-of-week/get-start-of-week.ts
@@ -2,19 +2,10 @@ import type { FirstDayOfWeek } from '../../types';
 
 export function getStartOfWeek(date: Date, firstDayOfWeek: FirstDayOfWeek = 1) {
   const value = new Date(date);
-  const day = value.getDay();
 
-  if (day === firstDayOfWeek) {
-    return value;
+  while (value.getDay() !== firstDayOfWeek) {
+    value.setDate(value.getDate() - 1);
   }
 
-  if (day < firstDayOfWeek) {
-    const diff = day - firstDayOfWeek;
-    value.setDate(value.getDate() - (day - diff));
-    return value;
-  }
-
-  const diff = firstDayOfWeek - day;
-  value.setDate(value.getDate() + diff);
   return value;
 }
