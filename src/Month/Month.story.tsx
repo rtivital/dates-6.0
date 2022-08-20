633120ae6b85e466cb5778f7518f783cda45e89a
Vitaly Rtishchev <rtivital@gmail.com>
2022-08-20 23:04:04 +0300

    Add basic styles api support to Month

---
+++ src/Month/Month.story.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Month } from './Month';
+
+export default { title: 'Month' };
+
+export function Usage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <Month />
+    </div>
+  );
+}
--- src/Month/Month.tsx
+++ src/Month/Month.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/no-unused-prop-types */
 import React, { forwardRef } from 'react';
 import { DefaultProps, Selectors, Box, useComponentDefaultProps } from '@mantine/core';
+import type { FirstDayOfWeek } from '../types';
+import { WeekdaysRow, WeekdaysRowStylesNames } from '../WeekdaysRow';
 import useStyles from './Month.styles';
-import { WeekdaysRow } from '../WeekdaysRow';
 
-export type MonthStylesNames = Selectors<typeof useStyles>;
+export type MonthStylesNames = Selectors<typeof useStyles> | WeekdaysRowStylesNames;
 
 export interface MonthSettings {}
 
@@ -13,13 +14,31 @@ export interface MonthProps
     MonthSettings,
     React.ComponentPropsWithoutRef<'table'> {
   __staticSelector?: string;
+
+  /** dayjs locale, defaults to theme.datesLocale */
+  locale?: string;
+
+  /** number 0-6, 0 – Sunday, 6 – Saturday, defaults to 1 – Monday */
+  firstDayOfWeek?: FirstDayOfWeek;
+
+  /** dayjs format for weekdays names */
+  weekdayFormat?: string;
 }
 
 const defaultProps: Partial<MonthProps> = {};
 
 export const Month = forwardRef<HTMLTableElement, MonthProps>((props, ref) => {
-  const { className, classNames, styles, unstyled, __staticSelector, ...others } =
-    useComponentDefaultProps('Month', defaultProps, props);
+  const {
+    className,
+    classNames,
+    styles,
+    unstyled,
+    __staticSelector,
+    locale,
+    firstDayOfWeek,
+    weekdayFormat,
+    ...others
+  } = useComponentDefaultProps('Month', defaultProps, props);
 
   const { classes, cx } = useStyles(null, {
     classNames,
@@ -28,10 +47,22 @@ export const Month = forwardRef<HTMLTableElement, MonthProps>((props, ref) => {
     name: ['Month', __staticSelector],
   });
 
+  const stylesApiProps = {
+    classNames,
+    styles,
+    unstyled,
+    __staticSelector,
+  };
+
   return (
     <Box component="table" className={cx(classes.month, className)} ref={ref} {...others}>
       <thead>
-        <WeekdaysRow />
+        <WeekdaysRow
+          {...stylesApiProps}
+          locale={locale}
+          firstDayOfWeek={firstDayOfWeek}
+          format={weekdayFormat}
+        />
       </thead>
     </Box>
   );
