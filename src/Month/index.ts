35ad49bacc1138f432c2dafe0d111d7774b6a7ad
Vitaly Rtishchev <rtivital@gmail.com>
2022-08-20 21:41:21 +0300

    Add get-month-days function

--- src/Month/index.ts
+++ src/Month/index.ts
@@ -1,2 +1,3 @@
 export { getEndOfWeek } from './get-end-of-week/get-end-of-week';
 export { getStartOfWeek } from './get-start-of-week/get-start-of-week';
+export { getMonthDays } from './get-month-days/get-month-days';
