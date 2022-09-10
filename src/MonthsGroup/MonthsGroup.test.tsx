f825fd5f22d21eb97b4c771a9564929906f89b79
Vitaly Rtishchev <rtivital@gmail.com>
2022-09-10 11:06:44 +0400

    Add keyboard events tests to MonthsGroup

--- src/MonthsGroup/MonthsGroup.test.tsx
+++ src/MonthsGroup/MonthsGroup.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MonthsGroup, MonthsGroupProps } from './MonthsGroup';
 import { itSupportsMonthProps, itSupportsHeaderProps } from '../__tests__';
 
@@ -45,5 +46,64 @@ describe('@mantine/dates/MonthsGroup', () => {
       />
     );
     expect(screen.getByText('April 2022')).toHaveAttribute('aria-label', '3/2022');
+  });
+
+  it('handles arrow keyboard events correctly (numberOfMonths=1)', async () => {
+    const { container } = render(<MonthsGroup {...defaultProps} numberOfMonths={1} />);
+    const days = container.querySelectorAll('table button');
+
+    await userEvent.click(days[0]);
+    expect(days[0]).toHaveFocus();
+
+    await userEvent.type(days[0], '{ArrowRight}', { skipClick: true });
+    expect(days[1]).toHaveFocus();
+
+    await userEvent.type(days[1], '{ArrowDown}', { skipClick: true });
+    expect(days[8]).toHaveFocus();
+
+    await userEvent.type(days[8], '{ArrowLeft}', { skipClick: true });
+    expect(days[7]).toHaveFocus();
+
+    await userEvent.type(days[7], '{ArrowUp}', { skipClick: true });
+    expect(days[0]).toHaveFocus();
+  });
+
+  it('handles arrow keyboard events correctly (numberOfMonths=2)', async () => {
+    const { container } = render(<MonthsGroup {...defaultProps} numberOfMonths={2} />);
+    const months = container.querySelectorAll('.mantine-Month-month');
+    const firstMonthDays = months[0].querySelectorAll('button');
+    const secondMonthDays = months[1].querySelectorAll('button');
+
+    await userEvent.click(firstMonthDays[5]);
+    expect(firstMonthDays[5]).toHaveFocus();
+
+    await userEvent.type(firstMonthDays[5], '{ArrowRight}', { skipClick: true });
+    expect(firstMonthDays[6]).toHaveFocus();
+
+    await userEvent.type(firstMonthDays[6], '{ArrowRight}', { skipClick: true });
+    expect(secondMonthDays[0]).toHaveFocus();
+
+    await userEvent.type(secondMonthDays[0], '{ArrowDown}', { skipClick: true });
+    expect(secondMonthDays[7]).toHaveFocus();
+
+    await userEvent.type(secondMonthDays[7], '{ArrowLeft}', { skipClick: true });
+    expect(firstMonthDays[13]).toHaveFocus();
+  });
+
+  it('handles arrow keyboard events correctly at month edges', async () => {
+    const { container } = render(<MonthsGroup {...defaultProps} numberOfMonths={1} />);
+    const days = container.querySelectorAll('table button');
+
+    await userEvent.type(days[6], '{ArrowRight}');
+    expect(days[6]).toHaveFocus();
+
+    await userEvent.type(days[0], '{ArrowLeft}');
+    expect(days[0]).toHaveFocus();
+
+    await userEvent.type(days[0], '{ArrowUp}');
+    expect(days[0]).toHaveFocus();
+
+    await userEvent.type(days[days.length - 1], '{ArrowDown}');
+    expect(days[days.length - 1]).toHaveFocus();
   });
 });
